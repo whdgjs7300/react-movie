acd52c00e0ceb11e55c25779e18dd110fa11d460
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-23 12:35:27 +0900

    2/23 useState

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev)=>{
+    return prev + 1
+  })
   return (
     <div className="App">
-      
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
