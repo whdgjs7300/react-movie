1c6b98879f326cda58b1b5951dcb311788af6839
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-23 12:41:55 +0900

    2/23 useEffect

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev)=>{
     return prev + 1
   })
+  console.log('계속 렌더합니다');
+  // 데이터를 한번만 실행하게 만들어 줌
+  const iRunOnlyOnce = () =>{
+    console.log("힌번만 실행합니다");
+  }
+  useEffect(iRunOnlyOnce, []);
   return (
     <div className="App">
       <h1>{counter}</h1>
