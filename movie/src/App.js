8cc9ee78c24c9f592ab3d95e4a83973d3e11d423
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-24 20:25:41 +0900

    2/24 toDos

--- movie/src/App.js
+++ movie/src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (e)=> {
     setToDo(e.target.value);
   }
@@ -13,11 +14,13 @@ function App() {
       return;
     }
     setToDo("");
+    setToDos(currentArray => [toDo, ...currentArray])
   }
   console.log(toDo);
 
   return (
     <div className="App">
+      <h1>My toDos {toDos.length}</h1>
       <form onSubmit={onSubmit}>
       <input value={toDo} type="text" placeholder="할일을 입력하세요" onChange={onChange}/>
         <button type="">할일을 추가함</button>
