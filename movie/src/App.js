184e823023294b991202ce642571cca7536aa98e
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-27 17:31:02 +0900

    2/27 coins

--- movie/src/App.js
+++ movie/src/App.js
@@ -7,8 +7,11 @@ function App() {
   const [toDos, setToDos] = useState([]);
   // useEffect 연습
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(()=>{
-    fetch("http://api.coinpaprika.com/v1/tickers")
+    fetch("http://api.coinpaprika.com/v1/tickers").then(response => response.json())
+    .then(json => setCoins(json))
+    setLoading(false);
   }, [])
 
   const onChange = (e)=> {
@@ -37,9 +40,17 @@ function App() {
           return <li key={item}>{item}</li>
         })
       }
-    <h1>the Coins!</h1>
-    {loading ? <strong>Loading...</strong> : null}
-
+    <h1>the Coins! {loading ? "" : `(${coins.length})`}</h1>
+    {loading ? <strong>Loading...</strong> : (
+      <select>
+      {
+        coins.map((coin)=>{
+          return <option key={coin}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price}</option>
+        })
+      }
+    </select>
+    )}
+      
     </div>
   );
 }
