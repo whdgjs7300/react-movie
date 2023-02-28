a32b0fd0f901e091df168dd43d7fc73d696e2fcb
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-28 17:47:55 +0900

    2/28 movieApi

--- movie/src/App.js
+++ movie/src/App.js
@@ -7,13 +7,18 @@ import { useEffect, useState } from 'react';
 function App() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
+    const getMovies = async() => {
+      const response = await fetch(`
+        https://yts.mx/api/v2/list_movies.json?mininum_rating=8.8&sort_by=year`
+      );
+      const json = await response.json();
+      setMovies(json.data.movies);
+      setLoading(false);
+      }
+    
+
     useEffect(()=> {
-      fetch(`https://yts.mx/api/v2/list_movies.json?mininum_rating=8.8&sort_by=year`)
-      .then((response)=> response.json())
-      .then((json)=> {
-        setMovies(json.data.movies);
-        setLoading(false);
-      })
+      getMovies();
     }, [])
 
     console.log(movies)
@@ -21,7 +26,20 @@ function App() {
   return (
     <div className="App">
       {
-        loading ? <h1>Loading...</h1> : null
+        loading ? <h1>Loading...</h1> : <div>
+          {movies.map((movie)=>{
+            return <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt=""/>
+              <h1>{movie.title}</h1>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g)=>{
+                  <li key={g}>{g}</li>
+                })}
+              </ul>
+            </div>
+          })}
+        </div>
       }
 
       
