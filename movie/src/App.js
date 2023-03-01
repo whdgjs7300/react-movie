04844359a04dc43de929b5ad0eb97bcb06a084fc
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-01 14:04:47 +0900

    3/1 router

--- movie/src/App.js
+++ movie/src/App.js
@@ -2,46 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Todo from './components/todo';
 import Coin from './components/coin';
-import { useEffect, useState } from 'react';
-import Movie from './components/Movie';
+import Home from './routes/Home';
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async() => {
-      const response = await fetch(`
-        https://yts.mx/api/v2/list_movies.json?mininum_rating=8.8&sort_by=year`
-      );
-      const json = await response.json();
-      setMovies(json.data.movies);
-      setLoading(false);
-      }
-    
-
-    useEffect(()=> {
-      getMovies();
-    }, [])
-
-    console.log(movies)
 
   return (
-    <div className="App">
-    { loading ? (
-      <h1>Loading...</h1>
-    ) : (
-      <div>
-        {movies.map((movie)=>
-        <Movie
-        key={movie.id}
-        coverImg={movie.medium_cover_image}
-        title={movie.title} 
-        summary={movie.summary} 
-        genres={movie.genres}/>
-        )}
-      </div>
-    )
-  }
-    </div>
+    null
 )
 }
 export default App;
---
+++ movie/src/routes/Detail.jsx
@@ -0,0 +1,11 @@
+
+const Detail = () => {
+
+    return ( 
+        <div>
+            <h1>Detail</h1>
+        </div>
+    );
+}
+
+export default Detail;
